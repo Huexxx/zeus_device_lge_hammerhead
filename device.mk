f9a538546cd1151f64473410e096d1bed23a0a7a
Huexxx <huexxx@gmail.com>
2014-02-04 19:58:04 +0100

    Add f2fs utilities

--- device.mk
+++ device.mk
@@ -113,6 +113,13 @@ PRODUCT_CHARACTERISTICS := nosdcard
 DEVICE_PACKAGE_OVERLAYS := \
     device/lge/hammerhead/overlay
 
+# Utilities by Huexxx
+PRODUCT_PACKAGES += \
+    mkfs.f2fs \
+    fsck.f2fs \
+    fibmap.f2fs \
+    f2fstat
+
 # Live Wallpapers
 PRODUCT_PACKAGES += \
     LiveWallpapersPicker \
